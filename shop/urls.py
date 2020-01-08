80b81f2f0e73b280101d6b09f336f7c4deffefd1
Anuj Sharma <anujsharma6299@gmail.com>
2020-01-08 23:19:52 +0530

    Navbar for ECommerce

---
+++ shop/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'shop'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('<int:category_id>/', views.detail, name='detail'),
+
+]
